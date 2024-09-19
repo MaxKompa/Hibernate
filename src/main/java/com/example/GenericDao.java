10d5a40c65c8abd983f38f3a8245e663d3b2a638
MaxKompa <max.kompa645@gmail.com>
2024-09-19 19:33:23 +0200

    Initial commit

---
+++ src/main/java/com/example/GenericDao.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.util.List;
+
+public interface GenericDao<T, ID> {
+
+    void save(T entity);
+
+    T findById(ID id);
+
+    List<T> findAll();
+
+    T update(T entity);
+
+    boolean deleteById(ID id);
+}
